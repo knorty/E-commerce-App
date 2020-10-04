fc045bd7a354d7700d39e149f8f7cc2ed6d40517
Kyle Norton <kylebnorton@gmail.com>
2020-10-04 00:16:39 -0600

    Add files for home page

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React, { Component } from 'react';
+import HomePage from './homepage.component';
 import './App.css';
+import './homepage.styles.scss';
 
 class App extends Component {
   render() {
-    return <div className='App'>Hello World</div>;
+    return <HomePage />;
   }
 }
 
