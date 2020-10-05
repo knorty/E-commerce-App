984a5799133b339b12b7f0113c3d1d66673dbf53
Kyle Norton <kylebnorton@gmail.com>
2020-10-04 22:50:03 -0600

    Added routing for menu item

--- src/App.js
+++ src/App.js
@@ -1,10 +1,24 @@
 import React, { Component } from 'react';
+import { Switch, Route } from 'react-router-dom';
 import HomePage from './pages/homepage/homepage.component';
 import './App.css';
 
+const HatsPage = () => (
+  <div>
+    <h1>HATS PAGE</h1>
+  </div>
+)
+
 class App extends Component {
   render() {
-    return <HomePage />;
+    return (
+      <div>
+        <Switch>
+          <Route exact path='/' component={HomePage} />
+          <Route path='/shop/hats' component={HatsPage} />
+        </Switch>
+      </div>
+    )
   }
 }
 
